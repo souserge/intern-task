542212cbf1ef655911bb0aa2930780c7e2244eb3
Serge Korzh <serge.korzh@gmail.com>
2018-03-15 22:49:24 +0100

    Fix minor issues; add html demo

--- truncate.js
+++ truncate.js
@@ -1,13 +1,21 @@
+document.getElementById("truncate").addEventListener('click', () => {
+    const htmlStr = document.getElementById("htmlStr").value
+    const n = parseInt(document.getElementById("numChars").value)
+    const truncatedHtmlStr = truncate(htmlStr, n)
+    document.getElementById("truncatedHtmlStr").value = truncatedHtmlStr
+    document.getElementById("result").innerHTML = truncatedHtmlStr
+})
+
 
 const ellipsis = '…'
 function truncate(htmlStr, n) {
-    currPos = 0
-    numChars = 0
-    let tagStack = []
+    let currPos = 0
+    let numChars = 0
+    const tagStack = []
     while (currPos < htmlStr.length) {
-        const c = htmlStr[i]
+        const c = htmlStr[currPos]
         if (c === '<') {
-            let tag = readTag(htmlStr, currPos)
+            const tag = readTag(htmlStr, currPos)
             if (tag.isClosing) {
                 if (tagStack[tagStack.length - 1] === tag.tagname) {
                     tagStack.pop()
@@ -16,21 +24,13 @@ function truncate(htmlStr, n) {
             else {
                 tagStack.push(tag.tagname)
             }
-            currPos = tag.newPos
+            currPos = tag.newPos + 1
         }
         else {
-            let word = readWord(htmlStr, currPos)
-            let len = word.newPos - currPos
+            const word = readWord(htmlStr, currPos)
+            const len = word.newPos - currPos
             if (numChars + len >= n) {
-                let cutPoint = -1
-                const maxFromCurr = n - numChars
-                if (maxFromCurr <= word.spaceNum) {
-                    cutPoint = currPos + maxFromCurr
-                } else {
-                    cutPoint = currPos
-                }
-                htmlStr[cutPoint] = ellipsis
-                return appendClosingTags(htmlStr.slice(0, cutPoint+1), tagStack)
+                return appendClosingTags(htmlStr.slice(0, currPos) + ellipsis, tagStack)
             } else {
                 currPos = word.newPos
                 numChars += len
@@ -52,13 +52,12 @@ function readWord(htmlStr, currPos) {
     let {spaceNum, newPos} = readSpaces(htmlStr, currPos)
     while (newPos < htmlStr.length) {
         if (htmlStr[newPos] === ' ' || htmlStr[newPos] === '<') {
-            return {word, newPos, spaceNum}
+            break
         }
         word += htmlStr[newPos]
         newPos += 1
     }
-
-    return null
+    return {word, newPos, spaceNum}
 }
 
 function readSpaces(htmlStr, currPos) {
@@ -70,31 +69,35 @@ function readSpaces(htmlStr, currPos) {
 }
 
 function readTag(htmlStr, currPos) {
-    const isClosing = isClosingTag(htmlStr, currPos)
-    let {tagname, newPos} = readTagname(htmlStr, currPos+1+isClosing) // can be an error
+    let isClosing = isClosingTag(htmlStr, currPos)
+    let {tagname, newPos} = readTagname(htmlStr, currPos+1+isClosing)
 
     while (newPos < htmlStr.length) {
         if (htmlStr[newPos] === '>') {
-           return {tagname, newPos, isClosing} 
-        } //TODO: check for self-closing tag
+            if (htmlStr[newPos-1] === '/') { // self-closing tag
+                isClosing = true
+            }
+            break
+        }
         newPos += 1
     }
-    return null;
+    return {tagname, newPos, isClosing}
 }
 
 function isClosingTag(htmlStr, currPos) {
-    return (currPos+1 < htmlStr) 
-        && (htmlStr[currPos+1] === '/')
+    return (currPos+1 < htmlStr.length) && (htmlStr[currPos+1] === '/')
 }
 
 function readTagname(htmlStr, currPos) {
     let tagname = ''
     let newPos = currPos
     while (newPos < htmlStr.length) {
-        if (htmlStr[newPos] === ' ') {
-            return { tagname, newPos }
+        const c = htmlStr[newPos]
+        if (c === ' ' || c === '/' || c === '>') {
+            break
         }
         tagname += htmlStr[newPos]
         newPos += 1
     }
+    return { tagname, newPos }
 }
